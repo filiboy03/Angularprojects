c1a09a62110029cc915492d8443d987bedce3909
filiboy03 <filiboy03@gmail.com>
2017-09-21 21:13:15 -0500

    commitlab

---
+++ lab14/Appform/src/app/form.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+  FormArray
+} from "@angular/forms";
+import { Observable } from "rxjs/Rx";
+
+@Component({
+  selector: 'form-comp',
+  templateUrl: 'form.component.html'
+})
+export class FormComponent {
+  myForm: FormGroup;
+
+ 
+
+  constructor(private formBuilder: FormBuilder) {
+    // this.myForm = new FormGroup({
+    //   'userData': new FormGroup({
+    //     'username': new FormControl('Asaad', Validators.required),
+    //     'email': new FormControl('', [
+    //       Validators.required,
+    //       Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+    //     ])
+    //   }),
+    //   'password': new FormControl('', Validators.required),
+    //   'gender': new FormControl('male'),
+    //   'hobbies': new FormArray([
+    //     new FormControl('Cooking', Validators.required)
+    //   ])
+    // });
+
+    this.myForm = formBuilder.group({
+      'userData': formBuilder.group({
+        'username': ['Asaad', [Validators.required, this.exampleValidator]],
+        'email': ['', [
+          Validators.required,
+          Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+        ]]
+      }),
+      'post': ['', Validators.required],
+     
+      
+      
+    });
+
+  }
+
+  
+
+  onSubmit() {
+    console.log(this.myForm);
+  }
+
+  exampleValidator(control: FormControl): {[s: string]: boolean} {
+    if (control.value === 'Example') {
+      return {example: true};
+    }
+    return null;
+  }
+
+/*   asyncExampleValidator(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>(
+      (resolve, reject) => {
+        setTimeout(() => {
+          if (control.value === 'Example') {
+            resolve({'invalid': true});
+          } else {
+            resolve(null);
+          }
+        }, 1500);
+      }
+    );
+    return promise;
+  } */
+
+}
+
